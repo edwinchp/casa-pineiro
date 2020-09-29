912610382af20ebce1058675127bec965a84a548
Edwin Chi Pineiro <chipineiro@gmail.com>
2020-09-28 21:30:47 -0500

    edit completed also some lines formatted

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,39 +8,45 @@ use App\Http\Requests\ProductCreateRequest;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $products = Product::all();
         return view('product.admin_products')->with('products', $products);
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         $product = Product::findOrFail($id);
         return view('product.edit')->with('product', $product);
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         $product = Product::findOrFail($id);
-        return($product);//$product->update($request->all());
-        //return redirect()->route('product.index');
+        $product->update($request->all());
+        return redirect()->route('product.index');
     }
 
-    public function create(){
+    public function create()
+    {
         return view('product.create');
     }
 
-    public function store(ProductCreateRequest $request){
+    public function store(ProductCreateRequest $request)
+    {
         $product = Product::create($request->all());
         $product->save();
         return redirect()->route('product.index');
     }
 
-    public function show(){
-
+    public function show()
+    {
     }
 
-    public function search(Request $request){
-        
-        $products = Product::where('name', 'like', '%' .$request->get('searchRequest').'%')->get();
+    public function search(Request $request)
+    {
+
+        $products = Product::where('name', 'like', '%' . $request->get('searchRequest') . '%')->get();
         return json_encode($products);
     }
 }
