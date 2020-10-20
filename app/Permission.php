d141645282790646e4b42f8f48cfa54f53afa286
Edwin Chi Pineiro <chipineiro@gmail.com>
2020-10-19 19:55:31 -0500

    roles and permission $guarded

--- app/Permission.php
+++ app/Permission.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+
+    protected $guarded = [];
+
     public function roles()
     {
         return $this->belongsToMany(Role::class);
--- app/Role.php
+++ app/Role.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+
+    protected $guarded = [];
+
     public function permissions()
     {
         return $this->belongsToMany(Permission::class);
